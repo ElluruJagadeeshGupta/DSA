d543624088a220d9922ef754e982ba9f05e5dc04
ELLURU JAGADEESH GUPTA <satishillru@gmail.com>
2026-09-17 20:19:38 +0530

    Time: 0 ms (100%), Space: 46.3 MB (80.35%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] answer=new int[nums.length];
+        int index=0;
+        for(int i=0;i<n;i++)
+        {
+            answer[index++]=nums[i];
+            answer[index++]=nums[i+n];
+        }
+        return answer;
+
+    }
+}
