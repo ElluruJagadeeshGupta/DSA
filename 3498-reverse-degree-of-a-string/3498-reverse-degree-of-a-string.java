e2cca51197769556d8738c924ad643af132ae764
ELLURU JAGADEESH GUPTA <satishillru@gmail.com>
2026-09-20 20:55:46 +0530

    Time: 3 ms (8.39%), Space: 44.1 MB (34.29%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseDegree(String s) {
+       int sum=0;
+       for(int i=0;i<s.length();i++)
+       {
+        char ch=s.charAt(i);
+        int alphabet=ch-'a'+1;
+        int reverse=27-alphabet;
+        int pos=i+1;
+        int product=reverse*pos;
+        sum+=product;
+       } 
+       return sum;
+    }
+}
